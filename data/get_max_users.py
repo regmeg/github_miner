845bce4d007f41355586a249d008dce4bff4e271
multiks2200 <multiks2200@gmail.com>
2017-06-08 14:52:43 +0100

    added a smarter (timer) way to deal with the rate limit from Github API

--- data/get_max_users.py
+++ data/get_max_users.py
@@ -1,10 +1,14 @@
+#from __future__ import print_function
 import json,urllib
 import urllib2, base64
 import re
 from datetime import datetime
 import sys
 import argparse
-
+from urllib2 import HTTPError
+import pytz
+import time
+from tzlocal import get_localzone
 
 #import html2text
 
@@ -18,18 +22,64 @@ dig_arr = []
 
 max_digits=0
 
+utc = pytz.timezone('UTC')
+system_tz =  get_localzone()
+
+#fancy countdown function
+def countdown(t):
+	t = int(t)
+	while t:
+		mins, secs = divmod(t, 60)
+		timeformat = '{:02d}:{:02d}'.format(mins, secs)
+		#python2
+		#print('\r'+ timeformat, end="")
+		sys.stdout.write('\r'+ timeformat)
+		sys.stdout.flush()
+		#python3
+		#print(timeformat, end='\r')
+		time.sleep(1)
+		t -= 1
+
 def send_req(url, oAuthToken):
 	request = urllib2.Request(url)
-	request.add_header("Authorization", "token %s" % oAuthToken) 
-	response = urllib2.urlopen(request)
-	json_response=json.loads(response.read())
-	RateLimitLimit = response.info().getheader('X-RateLimit-Limit')
-	RateLimitRemaining = response.info().getheader('X-RateLimit-Remaining')
-	RateLimitReset = response.info().getheader('X-RateLimit-Reset')
+	request.add_header("Authorization", "token %s" % oAuthToken)
+	try:
+		response = urllib2.urlopen(request)
+		json_response=json.loads(response.read())
+		RateLimitLimit = response.info().getheader('X-RateLimit-Limit')
+		RateLimitRemaining = response.info().getheader('X-RateLimit-Remaining')
+		RateLimitReset = response.info().getheader('X-RateLimit-Reset')
+	except HTTPError as err:
+		print("Caught HTTPError: " + str(err))
+		RateLimitReset = err.headers['X-RateLimit-Reset']
+		
+		##put the requst to sleep until reset
+		now = system_tz.localize(datetime.now())
+		dt = datetime.utcfromtimestamp(int(RateLimitReset))
+		dt_utc = utc.localize(dt)
+		dt_sys = utc.localize(dt).astimezone(system_tz)
+		diff = dt_sys -now 
+		diff_sec = diff.total_seconds() + 10
+		print("Putting system to sleep for %s secs..." % diff_sec)
+		countdown(diff_sec)
+		print("...sending request again")
+		response = urllib2.urlopen(request)
+		json_response=json.loads(response.read())
+		RateLimitLimit = response.info().getheader('X-RateLimit-Limit')
+		RateLimitRemaining = response.info().getheader('X-RateLimit-Remaining')
+		RateLimitReset = response.info().getheader('X-RateLimit-Reset')
+		pass
+
+	dt = datetime.utcfromtimestamp(int(RateLimitReset))
+	dt_utc = utc.localize(dt)
+	dt_sys = utc.localize(dt).astimezone(system_tz)
 	print("RateLimit-Limit %s" % RateLimitLimit)
 	print("RateLimit-Remaining %s" % RateLimitRemaining)
-	print("RateLimit-Reset %s %s" %( RateLimitReset, datetime.utcfromtimestamp(1496917262).strftime('%Y-%m-%d %H:%M:%S')))
+	print("RateLimit-Reset %s" %( RateLimitReset))
+	print("RateLimit reset in UTC: %s" % dt_utc.strftime('%Y-%m-%d %H:%M:%S'))
+	print("RateLimit reset in system's %s : %s" % (system_tz.zone, dt_sys.strftime('%Y-%m-%d %H:%M:%S')))
 	print()
+
 	return_val = True if len(json_response) > 0 else False
 	return return_val
 
